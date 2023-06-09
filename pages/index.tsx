e5da40ffe8badd7d07de4fd079bd6c25203798c1
SJ0826 <ikosdu60@gmail.com>
2023-06-09 14:47:47 +0900

    feat: Header 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { NextPage } from "next";
-import { styled } from "styled-components";
 
 const index: NextPage = () => {
-  return <Container>hello world</Container>;
+  return (
+    <>
+      <div>hello world</div>
+    </>
+  );
 };
 
 export default index;
-
-const Container = styled.div`
-  font-size: 21px;
-  color: gray;
-`;
